3c50f5abcae3fa8c1869a44f91d42cb641e66230
CSC Hannover <csc@csc-hannover.de>
2026-06-13 10:57:54 +0000

    SW v5: Cache-Update erzwingen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'csc-pruef-v4';
+const CACHE = 'csc-pruef-v5';
 const ASSETS = ['./index.html','./style.css','./app.js','./config.js','./manifest.json'];
 
 // Install: neuen Cache befüllen
