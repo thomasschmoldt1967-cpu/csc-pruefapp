6b0608f7db6f45ff60b2901d0d1483bf70db219e
CSC Hannover <csc@csc-hannover.de>
2026-06-14 08:29:14 +0000

    Fix: Prüfer-Namen auf Handy sichtbar (autocomplete off → name)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'csc-pruef-v16';
+const CACHE = 'csc-pruef-v17';
 const ASSETS = ['./index.html','./style.css','./app.js','./config.js','./manifest.json','./logo.png'];
 
 // Install: neuen Cache befüllen
