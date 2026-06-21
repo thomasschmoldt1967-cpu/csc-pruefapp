218bd8411ed9df8a60588ee9df91d8238542f042
CSC Hannover <csc@csc-hannover.de>
2026-06-21 14:38:17 +0000

    v52: App-Buttons weißer Hintergrund

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'csc-pruef-v52';
+const CACHE = 'csc-pruef-v53';
 const ASSETS = ['./index.html','./style.css','./app.js','./config.js','./manifest.json','./logo.png','./firebase.js','./icon-192.png','./icon-512.png'];
 
 // Install: neuen Cache befüllen
