28ecbc795d6c7f71898fc4bab6803749342f9ff5
CSC Hannover <csc@csc-hannover.de>
2026-06-24 10:37:41 +0000

    v58: BA-Modal scrollbar (overflow-y:auto, flex-column, max-height)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'csc-pruef-v57';
+const CACHE = 'csc-pruef-v58';
 const ASSETS = ['./index.html','./style.css','./app.js','./config.js','./manifest.json','./logo.png','./firebase.js','./icon-192.png','./icon-512.png'];
 
 // Install: neuen Cache befüllen
